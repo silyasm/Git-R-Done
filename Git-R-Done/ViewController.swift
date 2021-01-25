567da988ee14210cc34a0aaa034b8602e0d4d0c8
Ilyas Munzir <ilyazmunzir@gmail.com>
2021-01-25 10:33:00 -0600

    Initial Commit

---
+++ Git-R-Done/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git-R-Done
+//
+//  Created by Student on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Git-R-DoneTests/Git_R_DoneTests.swift
@@ -0,0 +1,33 @@
+//
+//  Git_R_DoneTests.swift
+//  Git-R-DoneTests
+//
+//  Created by Student on 1/25/21.
+//
+
+import XCTest
+@testable import Git_R_Done
+
+class Git_R_DoneTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
